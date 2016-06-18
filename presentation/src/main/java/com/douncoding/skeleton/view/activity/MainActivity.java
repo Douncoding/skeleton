e765c1037179f385922eb7bed1f8d5425af5690d
douncoding <douncoding@gmail.com>
2016-06-18 20:05:05 +0900

    :data TestCode update#1

--- presentation/src/main/java/com/douncoding/skeleton/view/activity/MainActivity.java
+++ presentation/src/main/java/com/douncoding/skeleton/view/activity/MainActivity.java
@@ -1,11 +1,10 @@
 package com.douncoding.skeleton.view.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.douncoding.skeleton.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
